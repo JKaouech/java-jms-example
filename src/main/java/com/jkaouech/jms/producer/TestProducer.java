e9dc2a981756312db8c1e214cc670ca02f2c7f6e
jik <jik@LAPTOP-JIK2.oxia.corp>
2017-10-09 23:24:07 +0100

    Add producer

--- src/main/java/com/jkaouech/jms/producer/TestProducer.java
+++ src/main/java/com/jkaouech/jms/producer/TestProducer.java
@@ -1,27 +1,25 @@
 /**
  * 
  */
-package com.jkaouech.main;
+package com.jkaouech.jms.producer;
 
 import javax.jms.JMSException;
 
 import com.jkaouech.jms.producer.Producer;
+import com.jkaouech.jms.utility.ParamConnection;
 
 /**
  * @author Jihed KAOUECH
  *
  */
-public class Application {
+public class TestProducer {
 
-	private final static String BROKER_URL = "tcp://localhost:61616";
-	private final static String QUEUE = "request_queue";
-	
 	public static void main(String[] args) throws JMSException {
 		sendMsg();
 	}
 	
 	private static void sendMsg() {
-		Producer producter = new Producer(BROKER_URL, QUEUE);
+		Producer producter = new Producer(ParamConnection.BROKER_URL, ParamConnection.QUEUE);
 		producter.sendMessage("hello world");
 	}
 
---
+++ src/main/java/com/jkaouech/jms/utility/ParamConnection.java
@@ -0,0 +1,8 @@
+package com.jkaouech.jms.utility;
+
+public class ParamConnection {
+
+	public final static String BROKER_URL = "tcp://localhost:61616";
+	
+	public final static String QUEUE = "request_queue";
+}
